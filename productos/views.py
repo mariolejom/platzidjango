7f82237d1b64b593db5b93418ae0144cd49a7c70
Mario Moreno <malejo279@gmail.com>
2016-06-29 22:06:27 -0500

    Configuración de views.py para mostrar productos

--- productos/views.py
+++ productos/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Producto
 # Create your views here.
 
 def hello_world(request):
     #return HttpResponse("Hello World!")
-    return render(request, "index.html")
+    #return render(request, "index.html")
+    producto = Producto.objects.order_by('id')
+    template = loader.get_template('index.html')
+    context = {
+        'product': producto
+    }
+    return HttpResponse(template.render(request, context))

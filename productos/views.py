b9ec79bc7aaeebc3909ec6f45b50bc721d6904e4
Mario Moreno <malejo279@gmail.com>
2016-06-30 22:55:18 -0500

    Hacer link los productos, crear la vista y la url

--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,6 @@
 from django.template import loader
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Producto
 # Create your views here.
 
@@ -9,6 +9,14 @@ def hello_world(request):
     #return render(request, "index.html")
     producto = Producto.objects.order_by('id')
     template = loader.get_template('index.html')
+    context = {
+        'product': producto
+    }
+    return HttpResponse(template.render(context, request))
+
+def product_detail(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    template = loader.get_template('product_detail.html')
     context = {
         'product': producto
     }

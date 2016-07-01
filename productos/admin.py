6453192e69317f2fd9b3e5d198d7c2ac7c948a6e
Mario Moreno <malejo279@gmail.com>
2016-07-01 01:57:14 -0500

    Relación de modelos con Favorite

--- productos/admin.py
+++ productos/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Favorite
+
 # Register your models here.
 #admin.site.register(Producto)
 @admin.register(Producto)
 class AdminProducto(admin.ModelAdmin):
     list_display = ('name', 'category', 'descripcion',)
     list_filter = ('category',)
+
+@admin.register(Favorite)
+class AdminFavorite(admin.ModelAdmin):
+    list_display = ('user', 'product',)
+    list_filter = ('user', 'product',)

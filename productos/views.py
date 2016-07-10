bf72c0d03aef43495b840f1584071814dfeef1a4
Mario Moreno <malejo279@gmail.com>
2016-07-10 01:46:59 -0500

    Sistema básico de login

--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,11 @@
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import (
+    render,
+    get_object_or_404,
+    redirect)
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
@@ -49,3 +54,20 @@ def new_product(request):
     }
     return HttpResponse(template.render(context, request))
 
+def auth_login(request):
+    if request.method == 'POST':
+        action = request.POST.get('action', None)
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        if action == 'signup':
+            user = User.objects.create_user(username=username,
+                                            password=password)
+            user.save()
+        elif action == 'login':
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+
+    context = {}
+    return render(request, 'login/login.html', context)
